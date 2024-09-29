954d621135b245f7c48bff37f2523b9106257362
AbheestaV <abheesta@gmail.com>
2024-09-29 10:49:36 -0700

    create more templates

--- drive_app/forms.py
+++ drive_app/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from ..models import Restaurant
-from ..models import MenuItem
-from ..models import Order
-from ..models import Ride
-from ..models import Payment
+from .models import Restaurant
+from .models import MenuItem
+from .models import Order
+from .models import Ride
+from .models import Payment
 
 
 class RestaurantForm(forms.ModelForm):
